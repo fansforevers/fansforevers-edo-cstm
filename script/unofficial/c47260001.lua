03173ce97fdcd0a37b32f67213154f67c983cb73
fansforevers <53293104+fansforevers@users.noreply.github.com>
2022-06-23 06:22:44 +0700

    Update c47260001.lua

--- script/unofficial/c47260001.lua
+++ script/unofficial/c47260001.lua
@@ -35,7 +35,7 @@ end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetOperationInfo(0,CATEGORY_FUSION_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
 
