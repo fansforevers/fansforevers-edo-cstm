96b877a9e3d0f4efaf46f2e5dd91621e24150ee8
fansforevers <53293104+fansforevers@users.noreply.github.com>
2022-06-23 04:50:25 +0700

    Update c47260001.lua

--- script/unofficial/c47260001.lua
+++ script/unofficial/c47260001.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 
 	
 	--special summon
-	local e2=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
