a39504d2cc43f2f57f5d223218d4d6a70195bf5a
fansforevers <53293104+fansforevers@users.noreply.github.com>
2022-06-23 03:16:33 +0700

    Update c47260001.lua

--- script/unofficial/c47260001.lua
+++ script/unofficial/c47260001.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
-	Link.AddProcMixN(c,true,true,CARD_BLUEEYES_W_DRAGON,3)
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,CARD_BLUEEYES_W_DRAGON),3)
 
 
 	--link summon
