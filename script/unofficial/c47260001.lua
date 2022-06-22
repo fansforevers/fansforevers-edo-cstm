6558e572ec5334f176bbd3117a7f131b2865b908
fansforevers <53293104+fansforevers@users.noreply.github.com>
2022-06-23 03:44:17 +0700

    Update c47260001.lua

--- script/unofficial/c47260001.lua
+++ script/unofficial/c47260001.lua
@@ -1,11 +1,23 @@
---トラフィックゴースト
+-- Blue-Eyes Endless-God Dragon
 local s,id=GetID()
 function s.initial_effect(c)
-	--link summon
+	-- link summon
 	c:EnableReviveLimit()
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xdd),3)
-	
 
+	--Special Summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetCondition(s.sumcon)
+	e4:SetTarget(s.sumtg)
+	e4:SetOperation(s.sumop)
+	c:RegisterEffect(e4)
+	
+	
 	--link summon
 	-- c:EnableReviveLimit()
 	-- Link.AddProcedure(c,nil,3,3)
