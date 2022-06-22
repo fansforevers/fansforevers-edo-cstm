57c125af4b96ad529cb0c93c869c10ede815e54e
fansforevers <53293104+fansforevers@users.noreply.github.com>
2022-06-23 05:33:39 +0700

    Update c47260001.lua

--- script/unofficial/c47260001.lua
+++ script/unofficial/c47260001.lua
@@ -28,10 +28,7 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 end
 
 
-function s.spfilter(c,e,tp)
-	return (c:IsSetCard(0x1123) or (c:IsRace(RACE_DRAGON) and c:IsType(TYPE_SYNCHRO)))
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
+
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
